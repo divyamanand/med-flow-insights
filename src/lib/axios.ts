5ec41991ff3b8a0026e2d9810dcccd175e44749a
Divyam Anand <wrkdivan@gmail.com>
2025-11-20 09:44:12 +0530

    fix: update baseURL to use the correct secure API endpoint

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://13.201.19.12"
+const baseURL = import.meta.env.VITE_API_BASE_URL || "https://hospitalapi.isdiv.in"
 
 export const http = axios.create({
   baseURL: baseURL || '/',
